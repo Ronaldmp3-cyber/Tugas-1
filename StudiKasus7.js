62ef983ad2b06506bd45a1654ce9fc44f9fdcf40
Agustinus Ronald Leonardus J <agustinusronald589@gmail.com>
2026-07-20 16:28:59 +0800

    StudiKasus7

---
+++ StudiKasus7.js
@@ -0,0 +1,6 @@
+const nilai = [80, 95, 70, 88, 100, 65];
+let nilaiTerbesar = Math.max(...nilai);
+let nilaiTerkecil = Math.min(...nilai);;
+
+console.log("Nilai Terbesar: " + nilaiTerbesar);
+console.log("Nilai Terkecil: " + nilaiTerkecil);

c8ae15f14252c6c7f3cf2fd61cfe32280de126fb
Agustinus Ronald Leonardus J <agustinusronald589@gmail.com>
2026-07-22 08:22:41 +0800

    StudiKasus7

--- StudiKasus7.js
+++ StudiKasus7.js
@@ -1,6 +1,15 @@
 const nilai = [80, 95, 70, 88, 100, 65];
-let nilaiTerbesar = Math.max(...nilai);
-let nilaiTerkecil = Math.min(...nilai);;
+let nilaiTerbesar = nilai[0];
+let nilaiTerkecil = nilai[0];
+
+for (let i = 0; i < nilai.length; i++) {
+    if (nilai[i] > nilaiTerbesar) {
+        nilaiTerbesar = nilai[i];
+    }
+    if (nilai[i] < nilaiTerkecil) {
+        nilaiTerkecil = nilai[i];
+    }
+}
 
 console.log("Nilai Terbesar: " + nilaiTerbesar);
 console.log("Nilai Terkecil: " + nilaiTerkecil);
